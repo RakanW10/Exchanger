1282d19356caca53614c3336c7aed96b58c5315b
rakan <Rakan.w10@outlook.com>
2023-11-14 18:02:55 +0300

    Initial Commit

---
+++ Exchanger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Exchanger
+//
+//  Created by Rakan Alotaibi on 30/04/1445 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Exchanger/ExchangerApp.swift
@@ -0,0 +1,17 @@
+//
+//  ExchangerApp.swift
+//  Exchanger
+//
+//  Created by Rakan Alotaibi on 30/04/1445 AH.
+//
+
+import SwiftUI
+
+@main
+struct ExchangerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
